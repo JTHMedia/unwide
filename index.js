790bd67c8628554991068aaa99d5cc60687be3dd
J. T. Hartzfeld <jth@jthmedia.com>
2021-01-23 15:11:56 -0500

    Setup basic application structure.  Will be reworked.

---
+++ index.js
@@ -0,0 +1,5 @@
+
+nw.Window.open('/src/html/view-simple.html', {}, function(win) {
+	win.on('loaded', function() {
+	});
+});
---
+++ src/appmodel.js
@@ -0,0 +1,6 @@
+module.exports = (function(){
+	var Application = function() {
+		this.openFiles = [];
+		
+	}
+})();
